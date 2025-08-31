c9a75e88695f77a47f3736fb8f6b2f14defd9f45
Alejandra Romo <alejandragorettifigueroaromo@gmail.com>
2025-08-31 00:34:14 -0600

    fix(layout.tsx): change params type from synchronous to asynchronous to handle promise resolution correctly

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -41,7 +41,7 @@ export default async function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
 
